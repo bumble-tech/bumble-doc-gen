7ebf97d04bdd0bedc4e41b598f90b39dacbcbe65
Filipp Shcherbanich <filipp.shcherbanich@team.bumble.com>
2022-10-08 12:13:57 +0300

    Fixing breadcrumbs and adding docBlocks

--- BumbleDocGen/Render/Twig/Function/GeneratePageBreadcrumbs.php
+++ BumbleDocGen/Render/Twig/Function/GeneratePageBreadcrumbs.php
@@ -6,18 +6,31 @@ namespace BumbleDocGen\Render\Twig\Function;
 
 use BumbleDocGen\Render\Context\Context;
 
+/**
+ * Function to generate breadcrumbs on the page
+ */
 final class GeneratePageBreadcrumbs
 {
     public function __construct(private Context $context)
     {
     }
 
-    public function __invoke(string $currentPageTitle, string $templatePath, bool $fromCurrent = false): string
+    /**
+     * @param string $currentPageTitle Title of the current page
+     * @param string $templatePath Path to the template from which the breadcrumbs will be generated
+     * @param bool $skipFirstTemplatePage
+     *  If set to true, the page from which parsing starts will not participate in the formation of breadcrumbs
+     *  This option is useful when working with the _self value in a template, as it returns the full path to the
+     *  current template, and the reference to it in breadcrumbs should not be clickable.
+     *
+     * @return string
+     */
+    public function __invoke(string $currentPageTitle, string $templatePath, bool $skipFirstTemplatePage = true): string
     {
         return $this->context->getBreadcrumbsHelper()->renderBreadcrumbs(
             $currentPageTitle,
             $templatePath,
-            $fromCurrent
+            !$skipFirstTemplatePage
         );
     }
 }
