be7823ee43adf9401a54ceb7e9d4e3218e5c97c2
Filipp Shcherbanich <filipp.shcherbanich@team.bumble.com>
2022-10-12 13:13:38 +0300

    Adding demo

---
+++ demo/demo1/demoScript.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+<?php
+
+ini_set('memory_limit', '-1');
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$demoConfig = new class extends \BumbleDocGen\BaseConfiguration {
+    public function getProjectRoot(): string
+    {
+        return dirname(__DIR__, 2) . '/vendor/doctrine';
+    }
+
+    public function getTemplatesDir(): string
+    {
+        return __DIR__ . '/templates';
+    }
+
+    public function getOutputDirBaseUrl(): string
+    {
+        return '/demo/demo1/result';
+    }
+
+    public function getOutputDir(): string
+    {
+        return dirname(__DIR__, 2) . $this->getOutputDirBaseUrl();
+    }
+
+    public function getSourceLocators(): \BumbleDocGen\Parser\SourceLocator\SourceLocatorsCollection
+    {
+        return \BumbleDocGen\Parser\SourceLocator\SourceLocatorsCollection::create(
+            new \BumbleDocGen\Parser\SourceLocator\RecursiveDirectoriesSourceLocator([
+                $this->getProjectRoot(),
+            ]),
+        );
+    }
+
+    public function classEntityFilterCondition(\BumbleDocGen\Parser\Entity\ClassEntity $classEntity
+    ): \BumbleDocGen\Parser\FilterCondition\ConditionInterface {
+        return new \BumbleDocGen\Parser\FilterCondition\CommonFilterCondition\TrueCondition();
+    }
+};
+
+\BumbleDocGen\DocGenerator::generateDocumentation($demoConfig);
