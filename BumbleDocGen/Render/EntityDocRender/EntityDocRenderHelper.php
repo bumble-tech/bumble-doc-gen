522fe10bb77ad7c104abcb97760f47b3cad5c255
Filipp Shcherbanich <filipp.shcherbanich@team.bumble.com>
2022-11-09 19:35:43 +0300

    Improving getEntityUrlData method

--- BumbleDocGen/Render/EntityDocRender/EntityDocRenderHelper.php
+++ BumbleDocGen/Render/EntityDocRender/EntityDocRenderHelper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace BumbleDocGen\Render\EntityDocRender;
 
+use BumbleDocGen\Parser\AttributeParser;
 use BumbleDocGen\Parser\Entity\ClassEntity;
 use BumbleDocGen\Render\Context\Context;
 use BumbleDocGen\Render\Twig\Function\GetDocumentedClassUrl;
@@ -47,13 +48,35 @@ final class EntityDocRenderHelper
         }
         $className = $classData[0];
 
-        if (isset($pageLinks[$className])) {
+        $entity = $pageLinks[$className] ?? $pageLinks["\\$className"] ?? null;
+        if (!$entity) {
+            try {
+                $reflector = $context->getClassEntityCollection()->getReflector();
+                $reflectionClass = $reflector->reflectClass($className);
+                $attributeParser = new AttributeParser(
+                    $reflector, $context->getClassEntityCollection()->getLogger()
+                );
+                $entity = ClassEntity::create(
+                    $context->getConfiguration(),
+                    $reflector,
+                    $reflectionClass,
+                    $attributeParser
+                );
+            } catch (\Exception) {
+            }
+        }
+
+        if ($entity) {
             $cursor = '';
             if (isset($classData[1])) {
                 if (str_ends_with($classData[1], '()')) {
                     $cursor = 'm' . str_replace('()', '', $classData[1]);
                 } elseif (str_starts_with($classData[1], '$')) {
                     $cursor = 'p' . str_replace('$', '', $classData[1]);
+                } elseif ($entity->getMethodEntityCollection()->get($classData[1])) {
+                    $cursor = 'm' . $classData[1];
+                } elseif ($entity->getPropertyEntityCollection()->get($classData[1])) {
+                    $cursor = 'p' . $classData[1];
                 } else {
                     $cursor = 'q' . $classData[1];
                 }
@@ -62,16 +85,14 @@ final class EntityDocRenderHelper
             $getDocumentedClassUrl = new GetDocumentedClassUrl($context);
 
             if (in_array(self::CLASS_ENTITY_SHORT_LINK_OPTION, $linkOptions)) {
-                $linkString = $pageLinks[$className]->getShortName() .
-                    (isset($classData[1]) ? "::{$classData[1]}" : '');
+                $linkString = $entity->getShortName() . (isset($classData[1]) ? "::{$classData[1]}" : '');
             } elseif (in_array(self::CLASS_ENTITY_FULL_LINK_OPTION, $linkOptions)) {
-                $linkString = $pageLinks[$className]->getName() . (isset($classData[1]) ? "::{$classData[1]}" : '');
+                $linkString = $entity->getName() . (isset($classData[1]) ? "::{$classData[1]}" : '');
             } elseif (in_array(self::CLASS_ENTITY_ONLY_CURSOR_LINK_OPTION, $linkOptions) && isset($classData[1])) {
                 $linkString = $classData[1];
             }
 
-            $url = $getDocumentedClassUrl($pageLinks[$className]->getName(), $cursor);
-
+            $url = $getDocumentedClassUrl($entity->getName(), $cursor);
             return [
                 'url' => $url,
                 'title' => $linkString,
