9da8502f70e603846f586e8009eeb8ca3b69183e
Filipp Shcherbanich <filipp.shcherbanich@team.bumble.com>
2022-11-11 12:00:23 +0300

    Improving GetDocumentedClassUrl function - adding new param

--- BumbleDocGen/Render/Twig/Function/GetDocumentedClassUrl.php
+++ BumbleDocGen/Render/Twig/Function/GetDocumentedClassUrl.php
@@ -10,6 +10,8 @@ use BumbleDocGen\Render\Context\DocumentedEntityWrapper;
 use BumbleDocGen\Render\Context\DocumentedEntityWrappersCollection;
 use BumbleDocGen\Render\Twig\Filter\PrepareSourceLink;
 
+use function PHPUnit\Framework\matches;
+
 /**
  * Get the URL of a documented class by its name. If the class is found, next to the file where this method was called,
  * the `_Classes` directory will be created, in which the documented class file will be created
@@ -21,6 +23,7 @@ use BumbleDocGen\Render\Twig\Filter\PrepareSourceLink;
  *
  * @example {{ getDocumentedClassUrl('\\BumbleDocGen\\Render\\Twig\\MainExtension', 'getFunctions') }}
  * @example {{ getDocumentedClassUrl('\\BumbleDocGen\\Render\\Twig\\MainExtension') }}
+ * @example {{ getDocumentedClassUrl('\\BumbleDocGen\\Render\\Twig\\MainExtension', '', false) }}
  */
 final class GetDocumentedClassUrl
 {
@@ -39,34 +42,56 @@ final class GetDocumentedClassUrl
      *  If the class is not found, the DEFAULT_URL value will be returned.
      * @param string $cursor
      *  Cursor on the page of the documented class (for example, the name of a method or property)
+     * @param bool $createDocument
+     *  If true, creates a class document. Otherwise, just gives a reference to the class code
      *
      * @return string
      */
-    public function __invoke(string $className, string $cursor = ''): string
+    public function __invoke(string $className, string $cursor = '', bool $createDocument = true): string
     {
         $reflector = $this->context->getReflector();
         if (ParserHelper::isClassLoaded($reflector, $className)) {
             $classEntity = $this->context->getClassEntityCollection()->getEntityByClassName($className);
-            if (!is_null($classEntity)) {
+            if (!is_null($classEntity) && $createDocument) {
                 $documentedClass = new DocumentedEntityWrapper(
                     $classEntity, $this->context->getCurrentTemplateFilePatch()
                 );
                 $this->context->getEntityWrappersCollection()->add($documentedClass);
                 $url = $this->context->getConfiguration()->getOutputDirBaseUrl() . $documentedClass->getDocUrl();
             } else {
-                $configuration = $this->context->getConfiguration();
-                $reflection = $reflector->reflectClass($className);
-                $url = $reflection->getFileName() ? str_replace(
-                    $configuration->getProjectRoot(),
-                    '',
-                    $reflection->getFileName()
-                ) : '';
+                static $urlCaches = [];
+
+                $key = $className . $cursor;
+                if (array_key_exists($key, $urlCaches)) {
+                    $url = $urlCaches[$key];
+                } else {
+                    $configuration = $this->context->getConfiguration();
+                    $reflection = $reflector->reflectClass($className);
+                    $url = $reflection->getFileName() ? str_replace(
+                        $configuration->getProjectRoot(),
+                        '',
+                        $reflection->getFileName()
+                    ) : '';
+
+                    if ($url && mb_strlen($cursor) > 2) {
+                        $firstLetter = mb_substr($cursor, 0, 1);
+                        $cursor = ltrim($cursor, $firstLetter);
+                        try {
+                            $line = match ($firstLetter) {
+                                'm' => $reflection->getMethod($cursor)?->getStartLine(),
+                                'p' => $reflection->getProperty($cursor)?->getStartLine(),
+                                'q' => $reflection->getReflectionConstant($cursor)?->getStartLine(),
+                                default => 0,
+                            };
+                        } catch (\Exception) {
+                            $line = 0;
+                        }
+                        $url .= $line ? "L{$line}" : '';
+                    }
+                    $urlCaches[$key] = $url;
+                }
             }
 
-            if ($url && $cursor) {
-                $prepareSourceLink = new PrepareSourceLink();
-                return "{$url}#{$prepareSourceLink($cursor)}";
-            }
             return $url;
         }
         return self::DEFAULT_URL;
