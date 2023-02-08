aa3d13d499f06da47c24847c611056ef3c9699cc
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-02-08 06:59:05 +0300

    Adding method to get constant unsafe

--- BumbleDocGen/Parser/Entity/ConstantEntityCollection.php
+++ BumbleDocGen/Parser/Entity/ConstantEntityCollection.php
@@ -8,12 +8,15 @@ use BumbleDocGen\Parser\Entity\Cache\CacheableEntityWrapperFactory;
 
 final class ConstantEntityCollection extends BaseEntityCollection
 {
+    public function __construct(private ClassEntity $classEntity)
+    {
+    }
+
     public static function createByClassEntity(
         ClassEntity $classEntity
     ): ConstantEntityCollection
     {
-        $constantEntityCollection = new ConstantEntityCollection();
-
+        $constantEntityCollection = new ConstantEntityCollection($classEntity);
         foreach ($classEntity->getConstantsData() as $name => $constantData) {
             $constantEntity = CacheableEntityWrapperFactory::createConstantEntity(
                 $classEntity,
@@ -42,5 +45,22 @@ final class ConstantEntityCollection extends BaseEntityCollection
     public function get(string $key): ?ConstantEntity
     {
         return $this->entities[$key] ?? null;
+    }
+
+    public function unsafeGet(string $key): ?ConstantEntity
+    {
+        $constantEntity = $this->get($key);
+        if (!$constantEntity) {
+            $constantsData = $this->classEntity->getConstantsData()[$key] ?? null;
+            if (is_array($constantsData)) {
+                return CacheableEntityWrapperFactory::createConstantEntity(
+                    $this->classEntity,
+                    $key,
+                    $constantsData['declaringClass'],
+                    $constantsData['implementingClass']
+                );
+            }
+        }
+        return $constantEntity;
     }
 }
