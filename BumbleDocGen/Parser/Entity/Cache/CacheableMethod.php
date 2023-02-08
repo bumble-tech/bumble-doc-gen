9f6cc172e5ddc3e7433624865a109258bf86e46b
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-02-08 13:30:54 +0300

    Adding cache time constants

--- BumbleDocGen/Parser/Entity/Cache/CacheableMethod.php
+++ BumbleDocGen/Parser/Entity/Cache/CacheableMethod.php
@@ -9,7 +9,11 @@ use Attribute;
 #[Attribute]
 class CacheableMethod
 {
-    public function __construct(public int $cacheSeconds = 604800)
+    public const HOUR_SECONDS = 3600;
+    public const DAY_SECONDS = 86400;
+    public const MONTH_SECONDS = self::DAY_SECONDS * 30;
+
+    public function __construct(private int $cacheSeconds = self::MONTH_SECONDS)
     {
     }
 
