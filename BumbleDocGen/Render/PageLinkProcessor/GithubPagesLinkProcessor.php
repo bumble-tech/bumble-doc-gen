e4ca9e99195a56eefbbe4a1f2cd24c922f8d1630
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-02-10 18:36:49 +0300

    GH linker lifehack

--- BumbleDocGen/Render/PageLinkProcessor/GithubPagesLinkProcessor.php
+++ BumbleDocGen/Render/PageLinkProcessor/GithubPagesLinkProcessor.php
@@ -20,6 +20,7 @@ class GithubPagesLinkProcessor implements PageLinkProcessorInterface
 
     public function getAbsoluteUrl(string $relativeUrl): string
     {
+        $relativeUrl = str_replace('/readme.md', '/index.md', $relativeUrl);
         $relativeUrl = str_replace($this->docFilesExtensions, '.html', $relativeUrl);
         $link = str_replace('//', '/', "{$this->configuration->getOutputDirBaseUrl()}{$relativeUrl}");
         return str_replace(':/', '://', $link);
