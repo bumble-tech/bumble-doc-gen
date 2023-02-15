f353178ff5f2e9bc1f1900a042e3408eb93ac4b8
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-02-15 07:25:46 +0300

    Adding defaults urls

--- BumbleDocGen/Render/Twig/Filter/StrTypeToUrl.php
+++ BumbleDocGen/Render/Twig/Filter/StrTypeToUrl.php
@@ -20,6 +20,46 @@ final class StrTypeToUrl
     public const TEMPLATE_TYPE_HTML = 'html';
     public const TEMPLATE_TYPE_RST = 'rst';
 
+    private static array $builtInUrls = [
+        'array' => 'https://www.php.net/manual/en/language.types.array.php',
+        'int' => 'https://www.php.net/manual/en/language.types.integer.php',
+        'string' => 'https://www.php.net/manual/en/language.types.string.php',
+        'bool' => 'https://www.php.net/manual/en/language.types.boolean.php',
+        'boolean' => 'https://www.php.net/manual/en/language.types.boolean.php',
+        'null' => 'https://www.php.net/manual/en/language.types.null.php',
+        'NULL' => 'https://www.php.net/manual/en/language.types.null.php',
+        'mixed' => 'https://www.php.net/manual/en/language.types.mixed.php',
+        'void' => 'https://www.php.net/manual/en/language.types.void.php',
+        'self' => 'https://www.php.net/manual/en/language.types.object.php',
+        'static' => 'https://wiki.php.net/rfc/static_return_type',
+        'false' => 'https://www.php.net/manual/en/language.types.boolean.php',
+        'true' => 'https://www.php.net/manual/en/language.types.boolean.php',
+        'never' => 'https://www.php.net/manual/ru/language.types.never.php',
+        'object' => 'https://www.php.net/manual/en/language.types.object.php',
+        'float' => 'https://www.php.net/manual/en/language.types.float.php',
+        'callable' => 'https://www.php.net/manual/en/language.types.callable.php',
+        '[]' => 'https://www.php.net/manual/en/language.types.array.php',
+        '\Traversable' => 'https://www.php.net/manual/en/class.traversable.php',
+        '\Iterator' => 'https://www.php.net/manual/en/class.iterator.php',
+        '\IteratorAggregate' => 'https://www.php.net/manual/en/class.iteratoraggregate.php',
+        '\IteratorIterator' => 'https://www.php.net/manual/en/class.iteratoriterator.php',
+        '\OuterIterator' => 'https://www.php.net/manual/en/class.outeriterator.php',
+        '\RecursiveIterator' => 'https://www.php.net/manual/en/class.recursiveiterator.php',
+        '\SeekableIterator' => 'https://www.php.net/manual/en/class.seekableiterator.php',
+        '\SplObserver' => 'https://www.php.net/manual/en/class.splobserver.php',
+        '\SplSubject' => 'https://www.php.net/manual/en/class.splsubject.php',
+        '\Throwable' => 'https://www.php.net/manual/en/class.throwable.php',
+        '\ArrayAccess' => 'https://www.php.net/manual/en/class.arrayaccess.php',
+        '\Serializable' => 'https://www.php.net/manual/en/class.serializable.php',
+        '\Closure' => 'https://www.php.net/manual/en/class.closure.php',
+        '\Generator' => 'https://www.php.net/manual/en/language.generators.overview.php',
+        '\Countable' => 'https://www.php.net/manual/en/class.countable.php',
+        '\stdClass' => 'https://www.php.net/manual/en/class.stdclass.php',
+        '\WeakReference' => 'https://www.php.net/manual/en/class.weakreference.php',
+        '\WeakMap' => 'https://www.php.net/manual/en/class.weakmap.php',
+        '\Stringable' => 'https://www.php.net/manual/en/class.stringable.php',
+    ];
+
     /**
      * @param Context $context Render context
      */
@@ -48,6 +88,16 @@ final class StrTypeToUrl
         $preparedTypes = [];
         $types = explode('|', $text);
         foreach ($types as $type) {
+            if (array_key_exists($type, self::$builtInUrls)) {
+                $link = self::$builtInUrls[$type];
+                if ($templateType == self::TEMPLATE_TYPE_RST) {
+                    $preparedTypes[] = "`{$type} <{$link}>`_";
+                } else {
+                    $preparedTypes[] = "<a href='{$link}'>{$type}</a>";
+                }
+                continue;
+            }
+
             $entityClassOfLink = $this->context->getClassEntityCollection()->getLoadedOrCreateNew($type);
             if ($entityClassOfLink->classDataCanBeLoaded()) {
                 if ($entityClassOfLink->getAbsoluteFileName()) {
