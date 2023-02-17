7400502a6b44075d8038caa8eb7b79ac40c8ddaa
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-02-17 04:31:52 +0300

    Adding root entities collection

---
+++ BumbleDocGen/Parser/Entity/RootEntityCollection.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BumbleDocGen\Parser\Entity;
+
+abstract class RootEntityCollection extends BaseEntityCollection
+{
+    /**
+     * @var RootEntityInterface[]
+     */
+    protected array $entities = [];
+
+    public function get(string $objectId): ?RootEntityInterface
+    {
+        return $this->entities[$objectId] ?? null;
+    }
+}
