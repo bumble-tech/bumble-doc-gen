a90502de5c3044f64014bedb9abde506a58ff758
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-03-30 10:10:58 +0300

    Adding new function

--- BumbleDocGen/Core/utils.php
+++ BumbleDocGen/Core/utils.php
@@ -10,3 +10,16 @@ if (!function_exists('BumbleDocGen\Core\is_associative_array')) {
         return count(array_filter(array_keys($array), 'is_string')) > 0;
     }
 }
+
+if (!function_exists('BumbleDocGen\Core\bites_int_to_string')) {
+    function bites_int_to_string(int $bites): string
+    {
+        $i = 0;
+        while (floor($bites / 1024) > 0) {
+            $i++;
+            $bites /= 1024;
+        }
+        $name = ['bites', 'KB', 'MB'];
+        return round($bites, 2) . ' ' . $name[$i];
+    }
+}
