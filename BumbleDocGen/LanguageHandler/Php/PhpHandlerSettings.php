d7324b497834dd341a025b4952ec10592a5083bb
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-04-02 18:19:28 +0300

    Using new methods to get configuration values

--- BumbleDocGen/LanguageHandler/Php/PhpHandlerSettings.php
+++ BumbleDocGen/LanguageHandler/Php/PhpHandlerSettings.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace BumbleDocGen\LanguageHandler\Php;
 
 use BumbleDocGen\Core\Configuration\ConfigurationParameterBag;
-use BumbleDocGen\Core\Configuration\ValueTransformer\ValueToClassTransformer;
-use BumbleDocGen\Core\Parser\FilterCondition\CommonFilterCondition\TrueCondition;
+use BumbleDocGen\Core\Configuration\Exception\InvalidConfigurationParameterException;
+use BumbleDocGen\Core\Configuration\ValueGetter\BooleanValueGetter;
+use BumbleDocGen\Core\Configuration\ValueGetter\ClassListValueGetter;
+use BumbleDocGen\Core\Configuration\ValueGetter\ClassValueGetter;
+use BumbleDocGen\Core\Configuration\ValueGetter\StringValueGetter;
 use BumbleDocGen\Core\Parser\FilterCondition\ConditionInterface;
 use BumbleDocGen\Core\Render\EntityDocRender\EntityDocRenderInterface;
 use BumbleDocGen\Core\Render\EntityDocRender\EntityDocRendersCollection;
@@ -17,11 +20,14 @@ final class PhpHandlerSettings
     public const DEFAULT_SETTINGS_FILE = __DIR__ . '/phpHandlerDefaultSettings.yaml';
 
     public function __construct(
-        private ValueToClassTransformer   $valueToClassTransformer,
-        private ConfigurationParameterBag $parameterBag
+        ConfigurationParameterBag    $parameterBag,
+        private ClassListValueGetter $classListValueGetter,
+        private ClassValueGetter     $classValueGetter,
+        private BooleanValueGetter   $booleanValueGetter,
+        private StringValueGetter    $stringValueGetter
     )
     {
-        $this->parameterBag->addValueFromFileIfNotExists(
+        $parameterBag->addValueFromFileIfNotExists(
             self::SETTINGS_PREFIX,
             self::DEFAULT_SETTINGS_FILE,
         );
@@ -32,85 +38,114 @@ final class PhpHandlerSettings
         return self::SETTINGS_PREFIX . ".{$key}";
     }
 
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
     public function getClassEntityFilter(): ConditionInterface
     {
         static $classEntityFilter = null;
         if (!$classEntityFilter) {
-            $filterClassValue = $this->parameterBag->get($this->getSettingsKey('class_filter'));
-            $classEntityFilter = $this->valueToClassTransformer->transform($filterClassValue);
-            if (!$classEntityFilter) {
-                $classEntityFilter = new TrueCondition();
-            }
+            /** @var ConditionInterface $classEntityFilter */
+            $classEntityFilter = $this->classValueGetter->validateAndGet(
+                $this->getSettingsKey('class_filter'),
+                ConditionInterface::class
+            );
         }
         return $classEntityFilter;
     }
 
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
     public function getClassConstantEntityFilter(): ConditionInterface
     {
         static $constantEntityFilter = null;
         if (!$constantEntityFilter) {
-            $filterClassConstantValue = $this->parameterBag->get($this->getSettingsKey('class_constant_filter'));
-            $constantEntityFilter = $this->valueToClassTransformer->transform($filterClassConstantValue);
-            if (!$constantEntityFilter) {
-                $constantEntityFilter = new TrueCondition();
-            }
+            /** @var ConditionInterface $constantEntityFilter */
+            $constantEntityFilter = $this->classValueGetter->validateAndGet(
+                $this->getSettingsKey('class_constant_filter'),
+                ConditionInterface::class
+            );
         }
         return $constantEntityFilter;
     }
 
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
     public function getMethodEntityFilter(): ConditionInterface
     {
         static $methodEntityFilter = null;
         if (!$methodEntityFilter) {
-            $filterMethodValue = $this->parameterBag->get($this->getSettingsKey('method_filter'));
-            $methodEntityFilter = $this->valueToClassTransformer->transform($filterMethodValue);
-            if (!$methodEntityFilter) {
-                $methodEntityFilter = new TrueCondition();
-            }
+            /** @var ConditionInterface $methodEntityFilter */
+            $methodEntityFilter = $this->classValueGetter->validateAndGet(
+                $this->getSettingsKey('method_filter'),
+                ConditionInterface::class
+            );
         }
         return $methodEntityFilter;
     }
 
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
     public function getPropertyEntityFilter(): ConditionInterface
     {
         static $propertyEntityFilter = null;
         if (!$propertyEntityFilter) {
-            $filterPropertyValue = $this->parameterBag->get($this->getSettingsKey('property_filter'));
-            $propertyEntityFilter = $this->valueToClassTransformer->transform($filterPropertyValue);
-            if (!$propertyEntityFilter) {
-                $propertyEntityFilter = new TrueCondition();
-            }
+            /** @var ConditionInterface $propertyEntityFilter */
+            $propertyEntityFilter = $this->classValueGetter->validateAndGet(
+                $this->getSettingsKey('property_filter'),
+                ConditionInterface::class
+            );
         }
         return $propertyEntityFilter;
     }
 
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
     public function getEntityDocRendersCollection(): EntityDocRendersCollection
     {
         static $entityDocRendersCollection = null;
         if (!$entityDocRendersCollection) {
             $entityDocRendersCollection = new EntityDocRendersCollection();
-            $docRendersConfigs = $this->parameterBag->get($this->getSettingsKey('doc_renders'));
-            foreach ($docRendersConfigs as $docRendersConfig) {
-                $entityDocRender = $this->valueToClassTransformer->transform($docRendersConfig);
-                if ($entityDocRender instanceof EntityDocRenderInterface) {
-                    $entityDocRendersCollection->add($entityDocRender);
-                }
+            $entityDocRenders = $this->classListValueGetter->validateAndGet(
+                $this->getSettingsKey('doc_renders'),
+                EntityDocRenderInterface::class
+            );
+            foreach ($entityDocRenders as $entityDocRender) {
+                $entityDocRendersCollection->add($entityDocRender);
             }
         }
         return $entityDocRendersCollection;
     }
 
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
     public function getFileSourceBaseUrl(): ?string
     {
-        $fileSourceBaseUrl = $this->parameterBag->get($this->getSettingsKey('file_source_base_url'));
-        if (!is_string($fileSourceBaseUrl) && !is_null($fileSourceBaseUrl)) {
-            return null;
+        static $fileSourceBaseUrl = -1;
+        if ($fileSourceBaseUrl === -1) {
+            $fileSourceBaseUrl = $this->stringValueGetter->validateAndGet(
+                $this->getSettingsKey('file_source_base_url')
+            );
         }
         return $fileSourceBaseUrl;
     }
 
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
     public function asyncSourceLoadingEnabled(): bool
     {
-        return (bool)$this->parameterBag->get($this->getSettingsKey('async_source_loading_enabled'));
+        static $asyncSourceLoadingEnabled = null;
+        if (is_null($asyncSourceLoadingEnabled)) {
+            $asyncSourceLoadingEnabled = $this->booleanValueGetter->validateAndGet(
+                $this->getSettingsKey('async_source_loading_enabled')
+            );
+        }
+        return $asyncSourceLoadingEnabled;
     }
 }
