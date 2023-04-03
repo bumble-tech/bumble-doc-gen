3ff3151ddb98fd7e06da167a1354457ece3af940
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-04-03 10:47:15 +0300

    Adding configuration wrapper

---
+++ BumbleDocGen/Core/Configuration/Configuration.php
@@ -0,0 +1,227 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BumbleDocGen\Core\Configuration;
+
+use BumbleDocGen\ConfigurationInterface;
+use BumbleDocGen\Core\Configuration\Exception\InvalidConfigurationParameterException;
+use BumbleDocGen\Core\Configuration\ValueGetter\ClassListValueGetter;
+use BumbleDocGen\Core\Configuration\ValueGetter\ClassValueGetter;
+use BumbleDocGen\Core\Configuration\ValueGetter\StringValueGetter;
+use BumbleDocGen\Core\Parser\SourceLocator\SourceLocatorInterface;
+use BumbleDocGen\Core\Parser\SourceLocator\SourceLocatorsCollection;
+use BumbleDocGen\Core\Plugin\PluginInterface;
+use BumbleDocGen\Core\Plugin\PluginsCollection;
+use BumbleDocGen\Core\Render\PageLinkProcessor\PageLinkProcessorInterface;
+use BumbleDocGen\Core\Render\TemplateFiller\TemplateFillerInterface;
+use BumbleDocGen\Core\Render\TemplateFiller\TemplateFillersCollection;
+use BumbleDocGen\LanguageHandler\LanguageHandlerInterface;
+use BumbleDocGen\LanguageHandler\LanguageHandlersCollection;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+/**
+ * Configuration wrapper for project documentation
+ */
+final class Configuration implements ConfigurationInterface
+{
+    public const DEFAULT_SETTINGS_FILE = __DIR__ . '/defaultConfiguration.yaml';
+
+    public function __construct(
+        ConfigurationParameterBag    $parameterBag,
+        private LoggerInterface      $logger,
+        private StringValueGetter    $stringValueGetter,
+        private ClassListValueGetter $classListValueGetter,
+        private ClassValueGetter     $classValueGetter
+    )
+    {
+        $parameterBag->addValueFromFileIfNotExists(
+            '',
+            self::DEFAULT_SETTINGS_FILE,
+        );
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getProjectRoot(): string
+    {
+        static $projectRoot = null;
+        if (is_null($projectRoot)) {
+            $projectRoot = $this->stringValueGetter->validateAndGet('project_root', false);
+        }
+        return $projectRoot;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getSourceLocators(): SourceLocatorsCollection
+    {
+        static $cachedSourceLocatorsCollection = null;
+        if (!$cachedSourceLocatorsCollection) {
+            $sourceLocators = $this->classListValueGetter->validateAndGet(
+                'source_locators',
+                SourceLocatorInterface::class
+            );
+            $cachedSourceLocatorsCollection = SourceLocatorsCollection::create(...$sourceLocators);
+        }
+        return $cachedSourceLocatorsCollection;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getTemplatesDir(): string
+    {
+        static $templatesDir = null;
+        if (is_null($templatesDir)) {
+            $templatesDir = $this->stringValueGetter->validateAndGet('templates_dir', false);
+        }
+        return $templatesDir;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getOutputDir(): string
+    {
+        static $outputDir = null;
+        if (is_null($outputDir)) {
+            $outputDir = $this->stringValueGetter->validateAndGet('output_dir', false);
+        }
+        return $outputDir;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getOutputDirBaseUrl(): string
+    {
+        static $outputDirBaseUrl = null;
+        if (is_null($outputDirBaseUrl)) {
+            $outputDirBaseUrl = $this->stringValueGetter->validateAndGet('output_dir_base_url', false);
+        }
+        return $outputDirBaseUrl;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getLanguageHandlersCollection(): LanguageHandlersCollection
+    {
+        static $cachedLanguageHandlersCollection = null;
+        if (!$cachedLanguageHandlersCollection) {
+            $languageHandlers = $this->classListValueGetter->validateAndGet(
+                'language_handlers',
+                LanguageHandlerInterface::class,
+                false
+            );
+            $cachedLanguageHandlersCollection = LanguageHandlersCollection::create(...$languageHandlers);
+        }
+        return $cachedLanguageHandlersCollection;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getPlugins(): PluginsCollection
+    {
+        static $cachedPlugins = null;
+        if (!$cachedPlugins) {
+            $pluginsList = $this->classListValueGetter->validateAndGet(
+                'plugins',
+                PluginInterface::class
+            );
+            $cachedPlugins = PluginsCollection::create(...$pluginsList);
+        }
+        return $cachedPlugins;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getTemplateFillers(): TemplateFillersCollection
+    {
+        static $cachedTemplateFillersCollection = null;
+        if (!$cachedTemplateFillersCollection) {
+            $templateFillers = $this->classListValueGetter->validateAndGet(
+                'template_fillers',
+                TemplateFillerInterface::class
+            );
+            $cachedTemplateFillersCollection = TemplateFillersCollection::create(...$templateFillers);
+        }
+        return $cachedTemplateFillersCollection;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getCacheDir(): ?string
+    {
+        static $cacheDir = -1;
+        if ($cacheDir === -1) {
+            $cacheDir = $this->stringValueGetter->validateAndGet('cache_dir');
+        }
+        return $cacheDir;
+    }
+
+    public function getLogger(): LoggerInterface
+    {
+        return $this->logger;
+    }
+
+    private function getCacheItemPool(string $cacheNamespace): CacheItemPoolInterface
+    {
+        static $cache = [];
+        if (!isset($cache[$cacheNamespace])) {
+            $cache[$cacheNamespace] = new FilesystemAdapter($cacheNamespace, 604800, $this->getCacheDir());
+        }
+        return $cache[$cacheNamespace];
+    }
+
+    public function getSourceLocatorCacheItemPool(): CacheItemPoolInterface
+    {
+        return $this->getCacheItemPool('sourceLocator');
+    }
+
+    public function getEntityCacheItemPool(): CacheItemPoolInterface
+    {
+        return $this->getCacheItemPool('entity');
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getPageLinkProcessor(): PageLinkProcessorInterface
+    {
+        static $pageLinkProcessor = null;
+        if (is_null($pageLinkProcessor)) {
+            /** @var PageLinkProcessorInterface $pageLinkProcessor */
+            $pageLinkProcessor = $this->classValueGetter->validateAndGet(
+                'page_link_processor',
+                PageLinkProcessorInterface::class
+            );
+        }
+        return $pageLinkProcessor;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getGitClientPath(): string
+    {
+        static $gitClientPath = null;
+        if (is_null($gitClientPath)) {
+            $gitClientPath = $this->stringValueGetter->validateAndGet('git_client_path', false);
+        }
+        return $gitClientPath;
+    }
+
+    public function clearOutputDirBeforeDocGeneration(): bool
+    {
+        return true;
+    }
+}
