b6b40758a6fa0902534260b3aaf750033a19fa76
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-04-03 16:26:38 +0300

    Optimizing DrawDocumentedEntityLink function

--- BumbleDocGen/Core/Render/Twig/Function/DrawDocumentedEntityLink.php
+++ BumbleDocGen/Core/Render/Twig/Function/DrawDocumentedEntityLink.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace BumbleDocGen\Core\Render\Twig\Function;
 
+use BumbleDocGen\Core\Configuration\Exception\InvalidConfigurationParameterException;
 use BumbleDocGen\Core\Parser\Entity\RootEntityInterface;
-use BumbleDocGen\Core\Render\Context\Context;
 
 /**
  * Creates an entity link by object
@@ -18,10 +18,7 @@ use BumbleDocGen\Core\Render\Context\Context;
  */
 final class DrawDocumentedEntityLink implements CustomFunctionInterface
 {
-    /**
-     * @param Context $context Render context
-     */
-    public function __construct(private Context $context)
+    public function __construct(private GetDocumentedEntityUrl $getDocumentedEntityUrlFunction)
     {
     }
 
@@ -37,18 +34,18 @@ final class DrawDocumentedEntityLink implements CustomFunctionInterface
         ];
     }
 
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
     public function __invoke(
         RootEntityInterface $entity,
         string              $cursor = '',
         bool                $useShortName = true
     ): string
     {
-        $getDocumentedEntityUrlFunction = new GetDocumentedEntityUrl($this->context);
+        $getDocumentedEntityUrlFunction = $this->getDocumentedEntityUrlFunction;
         $url = $getDocumentedEntityUrlFunction($entity->getRootEntityCollection(), $entity->getName(), $cursor);
         $name = $useShortName ? $entity->getShortName() : $entity->getName();
-        if ($this->context->isCurrentTemplateRst()) {
-            return "`{$name} <{$url}>`_";
-        }
         return "<a href='{$url}'>{$name}</a>";
     }
 }
