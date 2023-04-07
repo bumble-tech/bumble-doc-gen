0b6f982b81189ac1df2f454e3cd7a8d4107cfc9d
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-04-07 17:32:24 +0300

    Using LocalObjectCache instead static variables

--- BumbleDocGen/Core/Parser/Entity/Cache/CacheableEntityWrapperFactory.php
+++ BumbleDocGen/Core/Parser/Entity/Cache/CacheableEntityWrapperFactory.php
@@ -4,50 +4,57 @@ declare(strict_types=1);
 
 namespace BumbleDocGen\Core\Parser\Entity\Cache;
 
+use BumbleDocGen\Core\Cache\LocalCache\Exception\InvalidCallContextException;
+use BumbleDocGen\Core\Cache\LocalCache\Exception\ObjectNotFoundException;
+use BumbleDocGen\Core\Cache\LocalCache\LocalObjectCache;
 use Nette\PhpGenerator\Parameter;
 
 final class CacheableEntityWrapperFactory
 {
-    public function __construct()
+    public function __construct(
+        private LocalObjectCache $localObjectCache
+    )
     {
     }
 
     public function createWrappedEntityClass(string $className, string $wrapperName): string
     {
-        static $entityWrapperClassNames = [];
+        try {
+            return $this->localObjectCache->getCurrentMethodCachedResult($wrapperName);
+        } catch (ObjectNotFoundException|InvalidCallContextException) {
+        }
 
-        if (!isset($entityWrapperClassNames[$wrapperName])) {
-            $namespaceName = 'BumbleDocGen\\Core\\Parser\\Entity\\Cache';
+        $namespaceName = 'BumbleDocGen\\Core\\Parser\\Entity\\Cache';
 
-            $namespace = new \Nette\PhpGenerator\PhpNamespace($namespaceName);
-            $class = $namespace->addClass($wrapperName);
-            $class->setExtends($className);
-            $class->addTrait(CacheableEntityWrapperTrait::class);
-            $class->addImplement(CacheableEntityWrapperInterface::class);
+        $namespace = new \Nette\PhpGenerator\PhpNamespace($namespaceName);
+        $class = $namespace->addClass($wrapperName);
+        $class->setExtends($className);
+        $class->addTrait(CacheableEntityWrapperTrait::class);
+        $class->addImplement(CacheableEntityWrapperInterface::class);
 
-            $reflectionClass = new \ReflectionClass($className);
-            foreach ($reflectionClass->getMethods() as $method) {
-                if (!$method->isFinal() && !$method->isStatic()) {
-                    $cacheableMethodAttr = $method->getAttributes(CacheableMethod::class)[0] ?? null;
-                    if ($cacheableMethodAttr) {
-                        $newMethod = $class->addMethod($method->getName())
-                            ->setStatic($method->isStatic())
-                            ->setVariadic($method->isVariadic())
-                            ->setReturnType((string)$method->getReturnType());
+        $reflectionClass = new \ReflectionClass($className);
+        foreach ($reflectionClass->getMethods() as $method) {
+            if (!$method->isFinal() && !$method->isStatic()) {
+                $cacheableMethodAttr = $method->getAttributes(CacheableMethod::class)[0] ?? null;
+                if ($cacheableMethodAttr) {
+                    $newMethod = $class->addMethod($method->getName())
+                        ->setStatic($method->isStatic())
+                        ->setVariadic($method->isVariadic())
+                        ->setReturnType((string)$method->getReturnType());
 
-                        $parameters = [];
-                        foreach ($method->getParameters() as $parameter) {
-                            $parameter = new Parameter($parameter->getName());
-                            $parameter->setDefaultValue($parameter->getDefaultValue());
-                            $parameters[] = $parameter;
-                        }
-                        $newMethod->setParameters($parameters);
+                    $parameters = [];
+                    foreach ($method->getParameters() as $parameter) {
+                        $parameter = new Parameter($parameter->getName());
+                        $parameter->setDefaultValue($parameter->getDefaultValue());
+                        $parameters[] = $parameter;
+                    }
+                    $newMethod->setParameters($parameters);
 
-                        $cacheNamespace = "{$wrapperName}_{$method->getName()}";
+                    $cacheNamespace = "{$wrapperName}_{$method->getName()}";
 
-                        $cacheableMethodAttrObj = $cacheableMethodAttr->newInstance();
-                        $expiresAfter = time() + $cacheableMethodAttrObj->getCacheSeconds();
-                        $newMethod->setBody('
+                    $cacheableMethodAttrObj = $cacheableMethodAttr->newInstance();
+                    $expiresAfter = time() + $cacheableMethodAttrObj->getCacheSeconds();
+                    $newMethod->setBody('
                             $funcArgs = func_get_args();
                             $cacheKey = \\' . $cacheableMethodAttrObj->getCacheKeyGeneratorClass() . '::generateKey(
                                 \'' . $cacheNamespace . '\',
@@ -68,13 +75,13 @@ final class CacheableEntityWrapperFactory
                             }
                             return $result[$internalDataKey];
                         ');
-                    }
                 }
             }
-
-            eval((string)$namespace);
-            $entityWrapperClassNames[$wrapperName] = "{$namespaceName}\\$wrapperName";
         }
-        return $entityWrapperClassNames[$wrapperName];
+
+        eval((string)$namespace);
+        $entityWrapperClassName = "{$namespaceName}\\$wrapperName";
+        $this->localObjectCache->cacheCurrentMethodResultSilently($wrapperName, $entityWrapperClassName);
+        return $entityWrapperClassName;
     }
 }
