4b6fcf62006652a7997c06d706cc0c2ad483a874
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-04-14 12:35:11 +0300

    Adding class to work with Shared compressed document file cache

---
+++ BumbleDocGen/Core/Cache/SharedCompressedDocumentFileCache.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BumbleDocGen\Core\Cache;
+
+use BumbleDocGen\Core\Configuration\Configuration;
+use BumbleDocGen\Core\Configuration\Exception\InvalidConfigurationParameterException;
+
+final class SharedCompressedDocumentFileCache
+{
+    private const FILE_NAME = 'shared_c.cache';
+
+    private string $cacheFileName;
+    private array $cacheData = [];
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function __construct(
+        private Configuration $configuration
+    )
+    {
+        $this->cacheFileName = $this->configuration->getOutputDir() . '/' . self::FILE_NAME;
+        try {
+            $this->cacheData = unserialize(gzuncompress(file_get_contents($this->cacheFileName))) ?: [];
+        } catch (\Exception) {
+        }
+    }
+
+    public function getCacheFileName(): string
+    {
+        return $this->cacheFileName;
+    }
+
+    public function get(string $key, mixed $defaultValue = null): mixed
+    {
+        return $this->cacheData[$key] ?? $defaultValue;
+    }
+
+    public function set(string $key, mixed $data): void
+    {
+        $this->cacheData[$key] = $data;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
+     */
+    public function saveChanges(): void
+    {
+        $gitAttributesFile = $this->configuration->getOutputDir() . '/.gitattributes';
+        file_put_contents($gitAttributesFile, self::FILE_NAME . ' merge=ours');
+        file_put_contents($this->cacheFileName, gzcompress(serialize($this->cacheData)));
+    }
+}
