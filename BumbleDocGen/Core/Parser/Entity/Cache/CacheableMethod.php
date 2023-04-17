eee1e4423ecaf62fdbc094f3e812c0d22fcefbe2
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-04-17 11:23:32 +0300

    Adding additional checks

--- BumbleDocGen/Core/Parser/Entity/Cache/CacheableMethod.php
+++ BumbleDocGen/Core/Parser/Entity/Cache/CacheableMethod.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace BumbleDocGen\Core\Parser\Entity\Cache;
 
 use Attribute;
+use BumbleDocGen\Core\Parser\Entity\Cache\CacheKey\CacheKeyGeneratorInterface;
 use BumbleDocGen\Core\Parser\Entity\Cache\CacheKey\DefaultCacheKeyGenerator;
 
-#[Attribute]
-class CacheableMethod
+#[Attribute(Attribute::TARGET_METHOD)]
+final class CacheableMethod
 {
     public const HOUR_SECONDS = 3600;
-    public const DAY_SECONDS = 86400;
+    public const DAY_SECONDS = self::HOUR_SECONDS * 24;
     public const MONTH_SECONDS = self::DAY_SECONDS * 30;
 
     public function __construct(
@@ -19,6 +20,12 @@ class CacheableMethod
         private string $cacheKeyGeneratorClass = DefaultCacheKeyGenerator::class
     )
     {
+        if (!is_a($cacheKeyGeneratorClass, CacheKeyGeneratorInterface::class, true)) {
+            throw new \InvalidArgumentException(
+                'Argument $cacheKeyGeneratorClass must implement the ' . CacheKeyGeneratorInterface::class . ' interface. ' .
+                "Invalid value: `{$cacheKeyGeneratorClass}`"
+            );
+        }
     }
 
     public function getCacheSeconds(): int
