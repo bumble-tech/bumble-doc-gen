0bcc02bda3df79b8fce464d85663e2ac4476f705
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-04-19 18:14:02 +0300

    Do not handle exceptions

--- BumbleDocGen/Core/Configuration/ValueTransformer/ValueToClassTransformer.php
+++ BumbleDocGen/Core/Configuration/ValueTransformer/ValueToClassTransformer.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace BumbleDocGen\Core\Configuration\ValueTransformer;
 
 use DI\Container;
-use Psr\Log\LoggerInterface;
+use DI\DependencyException;
+use DI\NotFoundException;
 use function BumbleDocGen\Core\is_associative_array;
 
 final class ValueToClassTransformer implements ValueTransformerInterface
 {
-    public function __construct(private Container $diContainer, private LoggerInterface $logger)
+    public function __construct(private Container $diContainer)
     {
     }
 
@@ -19,31 +20,30 @@ final class ValueToClassTransformer implements ValueTransformerInterface
         return is_array($value) && isset($value['class']) && class_exists($value['class']);
     }
 
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
     public function transform(mixed $value): ?object
     {
         if (!$this->canTransform($value)) {
             return null;
         }
 
-        try {
-            if (!isset($value['arguments'])) {
-                return $this->diContainer->get($value['class']);
-            }
-
-            $arguments = [];
-            foreach ($value['arguments'] as $k => $argument) {
-                if ($this->canTransform($argument)) {
-                    $argument = $this->transform($argument);
-                }
-                $arguments[$k] = $argument;
-            }
-            if (!is_associative_array($arguments)) {
-                return new $value['class'](...$arguments);
-            }
-            return $this->diContainer->make($value['class'], $arguments);
-        } catch (\Exception $e) {
-            $this->logger->error($e->getMessage());
+        if (!isset($value['arguments'])) {
+            return $this->diContainer->get($value['class']);
         }
-        return null;
+
+        $arguments = [];
+        foreach ($value['arguments'] as $k => $argument) {
+            if ($this->canTransform($argument)) {
+                $argument = $this->transform($argument);
+            }
+            $arguments[$k] = $argument;
+        }
+        if (!is_associative_array($arguments)) {
+            return new $value['class'](...$arguments);
+        }
+        return $this->diContainer->make($value['class'], $arguments);
     }
 }
