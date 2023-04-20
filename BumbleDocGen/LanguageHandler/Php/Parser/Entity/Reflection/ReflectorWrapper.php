48235c6aa1b12030ce99c0d1b47aacbf8dda883d
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-04-20 12:33:35 +0300

    Fixing reflections to avoid circular dependency error

--- BumbleDocGen/LanguageHandler/Php/Parser/Entity/Reflection/ReflectorWrapper.php
+++ BumbleDocGen/LanguageHandler/Php/Parser/Entity/Reflection/ReflectorWrapper.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BumbleDocGen\LanguageHandler\Php\Parser\Entity\Reflection;
 
 use BumbleDocGen\Core\Cache\SourceLocatorCacheItemPool;
@@ -10,6 +12,8 @@ use BumbleDocGen\Core\Plugin\PluginEventDispatcher;
 use BumbleDocGen\LanguageHandler\Php\Parser\SourceLocator\CustomSourceLocatorInterface;
 use BumbleDocGen\LanguageHandler\Php\Parser\SourceLocator\Internal\CachedSourceLocator;
 use BumbleDocGen\LanguageHandler\Php\PhpHandlerSettings;
+use DI\DependencyException;
+use DI\NotFoundException;
 use Roave\BetterReflection\BetterReflection;
 use Roave\BetterReflection\Reflection\ReflectionClass;
 use Roave\BetterReflection\Reflection\ReflectionConstant;
@@ -20,73 +24,112 @@ use Roave\BetterReflection\SourceLocator\Type\AggregateSourceLocator;
 
 final class ReflectorWrapper implements Reflector
 {
-    private DefaultReflector $reflector;
+    private ?DefaultReflector $reflector = null;
 
-    /**
-     * @throws InvalidConfigurationParameterException
-     */
     public function __construct(
-        Configuration              $configuration,
-        PhpHandlerSettings         $phpHandlerSettings,
-        PluginEventDispatcher      $pluginEventDispatcher,
-        BetterReflection           $betterReflection,
-        SourceLocatorCacheItemPool $sourceLocatorCache
+        private Configuration              $configuration,
+        private PhpHandlerSettings         $phpHandlerSettings,
+        private PluginEventDispatcher      $pluginEventDispatcher,
+        private BetterReflection           $betterReflection,
+        private SourceLocatorCacheItemPool $sourceLocatorCache
     )
     {
-        $sourceLocatorsCollection = $pluginEventDispatcher->dispatch(
-            new OnLoadSourceLocatorsCollection($configuration->getSourceLocators())
-        )->getSourceLocatorsCollection();
-
-        $locator = $betterReflection->astLocator();
-
-        foreach ($sourceLocatorsCollection as $sourceLocator) {
-            if (is_a($sourceLocator, CustomSourceLocatorInterface::class, true)) {
-                $sourceLocators[] = $sourceLocator->getSourceLocator($locator);
-            }
-        }
-
-        if (!$phpHandlerSettings->asyncSourceLoadingEnabled()) {
-            $sourceLocators[] = new \Roave\BetterReflection\SourceLocator\Type\FileIteratorSourceLocator(
-                new \ArrayIterator(iterator_to_array($sourceLocatorsCollection->getCommonFinder()->getIterator())),
-                $locator
-            );
-        }
-        $sourceLocators[] = $betterReflection->sourceLocator();
-        $sourceLocator = new CachedSourceLocator(
-            new AggregateSourceLocator($sourceLocators),
-            $configuration,
-            $sourceLocatorCache
-        );
-        $this->reflector = new DefaultReflector($sourceLocator);
     }
 
+    /**
+     * @throws DependencyException
+     * @throws InvalidConfigurationParameterException
+     * @throws NotFoundException
+     */
+    private function getReflector(): DefaultReflector
+    {
+        if (!$this->reflector) {
+            $sourceLocatorsCollection = $this->pluginEventDispatcher->dispatch(
+                new OnLoadSourceLocatorsCollection($this->configuration->getSourceLocators())
+            )->getSourceLocatorsCollection();
+
+            $locator = $this->betterReflection->astLocator();
+
+            foreach ($sourceLocatorsCollection as $sourceLocator) {
+                if (is_a($sourceLocator, CustomSourceLocatorInterface::class, true)) {
+                    $sourceLocators[] = $sourceLocator->getSourceLocator($locator);
+                }
+            }
+
+            if (!$this->phpHandlerSettings->asyncSourceLoadingEnabled()) {
+                $sourceLocators[] = new \Roave\BetterReflection\SourceLocator\Type\FileIteratorSourceLocator(
+                    new \ArrayIterator(iterator_to_array($sourceLocatorsCollection->getCommonFinder()->getIterator())),
+                    $locator
+                );
+            }
+            $sourceLocators[] = $this->betterReflection->sourceLocator();
+            $sourceLocator = new CachedSourceLocator(
+                new AggregateSourceLocator($sourceLocators),
+                $this->configuration,
+                $this->sourceLocatorCache
+            );
+            $this->reflector = new DefaultReflector($sourceLocator);
+        }
+        return $this->reflector;
+    }
+
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     * @throws InvalidConfigurationParameterException
+     */
     public function reflectClass(string $identifierName): ReflectionClass
     {
-        return $this->reflector->reflectClass($identifierName);
+        return $this->getReflector()->reflectClass($identifierName);
     }
 
+    /**
+     * @throws DependencyException
+     * @throws InvalidConfigurationParameterException
+     * @throws NotFoundException
+     */
     public function reflectAllClasses(): iterable
     {
-        return $this->reflector->reflectAllClasses();
+        return $this->getReflector()->reflectAllClasses();
     }
 
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     * @throws InvalidConfigurationParameterException
+     */
     public function reflectFunction(string $identifierName): ReflectionFunction
     {
-        return $this->reflector->reflectFunction($identifierName);
+        return $this->getReflector()->reflectFunction($identifierName);
     }
 
+    /**
+     * @throws DependencyException
+     * @throws InvalidConfigurationParameterException
+     * @throws NotFoundException
+     */
     public function reflectAllFunctions(): iterable
     {
-        return $this->reflector->reflectAllFunctions();
+        return $this->getReflector()->reflectAllFunctions();
     }
 
+    /**
+     * @throws DependencyException
+     * @throws InvalidConfigurationParameterException
+     * @throws NotFoundException
+     */
     public function reflectConstant(string $identifierName): ReflectionConstant
     {
-        return $this->reflector->reflectConstant($identifierName);
+        return $this->getReflector()->reflectConstant($identifierName);
     }
 
+    /**
+     * @throws DependencyException
+     * @throws InvalidConfigurationParameterException
+     * @throws NotFoundException
+     */
     public function reflectAllConstants(): iterable
     {
-        return $this->reflector->reflectAllConstants();
+        return $this->getReflector()->reflectAllConstants();
     }
 }
