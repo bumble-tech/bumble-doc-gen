21c9341a98c68e8bceb6b2ac3dc53b86f4facd16
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-05-06 15:32:39 +0300

    Adding new config param to skip not exists directories

--- BumbleDocGen/Core/Parser/SourceLocator/RecursiveDirectoriesSourceLocator.php
+++ BumbleDocGen/Core/Parser/SourceLocator/RecursiveDirectoriesSourceLocator.php
@@ -11,12 +11,21 @@ final class RecursiveDirectoriesSourceLocator extends BaseSourceLocator
 {
     public function __construct(
         array $directories,
-        array $exclude = []
+        array $exclude = [],
+        bool  $abortExecutionIfPartOfDirsNotExists = true,
     )
     {
         parent::__construct();
+
+        if (!$abortExecutionIfPartOfDirsNotExists) {
+            $directories = array_filter($directories, 'is_dir');
+            if (!$directories) {
+                throw new \InvalidArgumentException("All specified directories do not exist");
+            }
+        }
+
         $directories = array_map(function (string $directory) {
-            if(!is_dir($directory)){
+            if (!is_dir($directory)) {
                 throw new \InvalidArgumentException("Directory `{$directory}` not found");
             }
             return realpath($directory);
