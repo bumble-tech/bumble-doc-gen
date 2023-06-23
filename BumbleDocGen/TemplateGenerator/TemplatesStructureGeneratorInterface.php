4dc09f09c64821726dd09263f815ad4be7047386
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-06-23 12:39:58 +0300

    Adding structure generation method signature

--- BumbleDocGen/TemplateGenerator/TemplatesStructureGeneratorInterface.php
+++ BumbleDocGen/TemplateGenerator/TemplatesStructureGeneratorInterface.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace BumbleDocGen\TemplateGenerator;
 
+use BumbleDocGen\Core\Parser\Entity\RootEntityCollection;
+
 interface TemplatesStructureGeneratorInterface
 {
+    public function generateStructureByEntityCollection(
+        RootEntityCollection $rootEntityCollection,
+        ?string              $additionalPrompt = null,
+    ): array;
 }
