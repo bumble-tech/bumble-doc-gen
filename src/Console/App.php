90aef95516f49cb410e5d866197c9eb688680d90
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-09-27 14:14:09 +0300

    Skipping configuration checks

--- src/Console/App.php
+++ src/Console/App.php
@@ -5,21 +5,13 @@ declare(strict_types=1);
 namespace BumbleDocGen\Console;
 
 use BumbleDocGen\Console\Command\GenerateCommand;
-use BumbleDocGen\Core\Configuration\Exception\InvalidConfigurationParameterException;
 use BumbleDocGen\DocGeneratorFactory;
-use DI\DependencyException;
-use DI\NotFoundException;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputOption;
 
 class App extends Application
 {
-    /**
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws InvalidConfigurationParameterException
-     */
     public function __construct()
     {
         parent::__construct('Bumble Doc Gen', \BumbleDocGen\DocGenerator::VERSION);
@@ -38,18 +30,16 @@ class App extends Application
         $this->setExtraCommands();
     }
 
-    /**
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws InvalidConfigurationParameterException
-     */
     private function setExtraCommands(): void
     {
-        $input = new ArgvInput();
-        $input->bind($this->getDefinition());
-        $configuration = (new DocGeneratorFactory())->createConfiguration($input->getOption('config'));
-        foreach ($configuration->getAdditionalConsoleCommands() as $command) {
-            $this->add($command);
+        try {
+            $input = new ArgvInput();
+            $input->bind($this->getDefinition());
+            $configuration = (new DocGeneratorFactory())->createConfiguration($input->getOption('config'));
+            foreach ($configuration->getAdditionalConsoleCommands() as $command) {
+                $this->add($command);
+            }
+        } catch (\Exception) {
         }
     }
 }
