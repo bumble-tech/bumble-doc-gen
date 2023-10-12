abd24954f0b4098fcce68f307e0731cae9c86f95
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-10-12 13:34:33 +0300

    Mute LastPageCommitter plugin errors

--- src/Core/Plugin/CorePlugin/LastPageCommitter/LastPageCommitter.php
+++ src/Core/Plugin/CorePlugin/LastPageCommitter/LastPageCommitter.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace BumbleDocGen\Core\Plugin\CorePlugin\LastPageCommitter;
 
 use BumbleDocGen\Core\Configuration\Configuration;
-use BumbleDocGen\Core\Configuration\Exception\InvalidConfigurationParameterException;
 use BumbleDocGen\Core\Plugin\Event\Renderer\BeforeCreatingDocFile;
 use BumbleDocGen\Core\Plugin\PluginInterface;
 use BumbleDocGen\Core\Renderer\Context\RendererContext;
@@ -28,26 +27,26 @@ final class LastPageCommitter implements PluginInterface
         ];
     }
 
-    /**
-     * @throws InvalidConfigurationParameterException
-     */
     final public function beforeCreatingDocFile(BeforeCreatingDocFile $event): void
     {
-        $gitClientPath = $this->configuration->getGitClientPath();
-        $filePath = str_replace(
-            "{$this->configuration->getProjectRoot()}/",
-            '',
-            "{$this->configuration->getTemplatesDir()}{$this->context->getCurrentTemplateFilePatch()}"
-        );
-        exec("{$gitClientPath} log --no-merges -1 {$filePath}", $output);
+        try {
+            $gitClientPath = $this->configuration->getGitClientPath();
+            $filePath = str_replace(
+                "{$this->configuration->getProjectRoot()}/",
+                '',
+                "{$this->configuration->getTemplatesDir()}{$this->context->getCurrentTemplateFilePatch()}"
+            );
+            exec("{$gitClientPath} log --no-merges -1 {$filePath}", $output);
 
-        $content = $event->getContent();
-        if (isset($output[2]) && str_contains($output[2], 'Date: ')) {
-            $author = str_replace('Author:', '<b>Last page committer:</b>', htmlspecialchars($output[1]));
-            $date = str_replace('Date:', '<b>Last modified date:</b>', $output[2]);
-            $contentRegenerationDate = '<b>Page content update date:</b> ' . date('D M d Y');
-            $content .= "\n\n<div id='page_committer_info'>\n<hr>\n{$author}<br>{$date}<br>{$contentRegenerationDate}<br>Made with <a href='https://github.com/bumble-tech/bumble-doc-gen/blob/master/docs/README.md'>Bumble Documentation Generator</a></div>";
+            $content = $event->getContent();
+            if (isset($output[2]) && str_contains($output[2], 'Date: ')) {
+                $author = str_replace('Author:', '<b>Last page committer:</b>', htmlspecialchars($output[1]));
+                $date = str_replace('Date:', '<b>Last modified date:</b>', $output[2]);
+                $contentRegenerationDate = '<b>Page content update date:</b> ' . date('D M d Y');
+                $content .= "\n\n<div id='page_committer_info'>\n<hr>\n{$author}<br>{$date}<br>{$contentRegenerationDate}<br>Made with <a href='https://github.com/bumble-tech/bumble-doc-gen/blob/master/docs/README.md'>Bumble Documentation Generator</a></div>";
+            }
+            $event->setContent($content);
+        } catch (\Exception) {
         }
-        $event->setContent($content);
     }
 }
