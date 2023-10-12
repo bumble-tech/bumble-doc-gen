599be210d734ac16597b766f97cabfc88704441f
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-10-12 13:36:38 +0300

    Changing MainTwigEnvironment create logic

--- src/Core/Renderer/Twig/MainTwigEnvironment.php
+++ src/Core/Renderer/Twig/MainTwigEnvironment.php
@@ -6,6 +6,8 @@ namespace BumbleDocGen\Core\Renderer\Twig;
 
 use BumbleDocGen\Core\Configuration\Configuration;
 use BumbleDocGen\Core\Configuration\Exception\InvalidConfigurationParameterException;
+use BumbleDocGen\Core\Plugin\Event\Renderer\OnCreateMainTwigEnvironment;
+use BumbleDocGen\Core\Plugin\PluginEventDispatcher;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -15,27 +17,39 @@ use Twig\Loader\FilesystemLoader;
 final class MainTwigEnvironment
 {
     private Environment $twig;
+    private bool $isEnvLoaded = false;
+
+    public function __construct(
+        private Configuration $configuration,
+        private MainExtension $mainExtension,
+        private PluginEventDispatcher $pluginEventDispatcher,
+    ) {
+    }
 
     /**
      * @throws InvalidConfigurationParameterException
      */
-    public function __construct(
-        Configuration $configuration,
-        MainExtension $mainExtension
-    ) {
-        $templateFolder = $configuration->getTemplatesDir();
-        $loader = new FilesystemLoader([$templateFolder]);
-        $this->twig = new Environment($loader);
-        $this->twig->addExtension($mainExtension);
+    private function loadMainTwigEnvironment(): void
+    {
+        if (!$this->isEnvLoaded) {
+            $templateFolder = $this->configuration->getTemplatesDir();
+            $loader = new FilesystemLoader([$templateFolder]);
+            $this->pluginEventDispatcher->dispatch(new OnCreateMainTwigEnvironment($loader));
+            $this->twig = new Environment($loader);
+            $this->twig->addExtension($this->mainExtension);
+            $this->isEnvLoaded = true;
+        }
     }
 
     /**
      * @throws SyntaxError
      * @throws RuntimeError
      * @throws LoaderError
+     * @throws InvalidConfigurationParameterException
      */
     public function render($name, array $context = []): string
     {
+        $this->loadMainTwigEnvironment();
         return $this->twig->render($name, $context);
     }
 }
