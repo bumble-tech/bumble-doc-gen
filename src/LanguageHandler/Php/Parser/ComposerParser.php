873078e4c1f777280587ec76f98c6f678a533840
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-11-10 17:38:06 +0300

    Adding new composer parser methods

--- src/LanguageHandler/Php/Parser/ComposerParser.php
+++ src/LanguageHandler/Php/Parser/ComposerParser.php
@@ -5,16 +5,61 @@ declare(strict_types=1);
 namespace BumbleDocGen\LanguageHandler\Php\Parser;
 
 use BumbleDocGen\Core\Configuration\Configuration;
+use BumbleDocGen\Core\Configuration\Exception\InvalidConfigurationParameterException;
+use Composer\Autoload\ClassLoader;
 
 final class ComposerParser
 {
     private array $packages = [];
+    private ?ClassLoader $classLoader = null;
+
     public function __construct(private Configuration $configuration)
     {
     }
 
     /**
-     * @throws \Exception
+     * @throws InvalidConfigurationParameterException
+     */
+    public function getComposerClassLoader(): ClassLoader
+    {
+        if ($this->classLoader) {
+            return $this->classLoader;
+        }
+
+        $classLoader = new ClassLoader();
+        $projectRoot = $this->configuration->getProjectRoot();
+        $composerJsonPath = "{$projectRoot}/composer.json";
+        if (!file_exists($composerJsonPath) || !is_readable($composerJsonPath)) {
+            return $classLoader;
+        }
+
+        $composerConfig = json_decode(file_get_contents($composerJsonPath), true);
+
+        $loadSection = function (array $section) use ($classLoader, $composerConfig, $projectRoot): void {
+            if (isset($section['psr-4'])) {
+                foreach ($section['psr-4'] as $namespace => $path) {
+                    $classLoader->addPsr4($namespace, "{$projectRoot}/{$path}");
+                }
+            }
+            if (isset($section['psr-0'])) {
+                foreach ($section['psr-0'] as $namespace => $path) {
+                    $classLoader->add($namespace, "{$projectRoot}/{$path}");
+                }
+            }
+        };
+
+        if (isset($composerConfig['autoload'])) {
+            $loadSection($composerConfig['autoload']);
+        }
+        if (isset($composerConfig['autoload-dev'])) {
+            $loadSection($composerConfig['autoload-dev']);
+        }
+        $this->classLoader = $classLoader;
+        return $classLoader;
+    }
+
+    /**
+     * @throws InvalidConfigurationParameterException
      */
     public function getComposerPackages(): array
     {
@@ -49,15 +94,24 @@ final class ComposerParser
         return $this->packages;
     }
 
-    /**
-     * @throws \Exception
-     */
     public function getComposerPackageDataByClassName(string $className): ?array
     {
         if (!ParserHelper::isCorrectClassName($className)) {
             return null;
         }
-        $packages = $this->getComposerPackages();
+        $packages = [];
+        try {
+            $packages = $this->getComposerPackages();
+        } catch (\Exception) {
+        }
+
+        if ($packages) {
+            $packages['Composer\\'] = [
+                "path" => "src/Composer/",
+                "namespace" => "Composer\\",
+                "url" => "https://github.com/composer/composer"
+            ];
+        }
         $classParts = explode('\\', $className);
         $namespace = '';
         foreach ($classParts as $part) {
