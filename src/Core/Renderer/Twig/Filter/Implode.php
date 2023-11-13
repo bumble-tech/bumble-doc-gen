192e0698aa778732678b3202c5fda98e17c83c80
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-11-14 00:52:59 +0300

    Adding comments

--- src/Core/Renderer/Twig/Filter/Implode.php
+++ src/Core/Renderer/Twig/Filter/Implode.php
@@ -3,10 +3,17 @@
 namespace BumbleDocGen\Core\Renderer\Twig\Filter;
 
 /**
+ * Join array elements with a string
+ *
  * @see https://www.php.net/manual/en/function.implode.php
  */
 final class Implode implements CustomFilterInterface
 {
+    /**
+     * @param array $elements The array to implode
+     * @param string $separator Element separator in result string
+     * @return string $result
+     */
     public function __invoke(array $elements, string $separator = ', '): string
     {
         return implode($separator, $elements);
