c485026315a97bcb80b2dcb7f054ce978c1f2119
fshcherbanich <filipp.shcherbanich@team.bumble.com>
2023-11-21 16:33:14 +0300

    Removing old code

--- src/LanguageHandler/Php/Parser/Entity/SubEntity/Method/MethodEntity.php
+++ src/LanguageHandler/Php/Parser/Entity/SubEntity/Method/MethodEntity.php
@@ -14,7 +14,6 @@ use BumbleDocGen\LanguageHandler\Php\Parser\Entity\PhpEntitiesCollection;
 use BumbleDocGen\LanguageHandler\Php\Parser\Entity\ClassLikeEntity;
 use BumbleDocGen\LanguageHandler\Php\Parser\ParserHelper;
 use BumbleDocGen\LanguageHandler\Php\Parser\PhpParser\NodeValueCompiler;
-use BumbleDocGen\LanguageHandler\Php\PhpHandlerSettings;
 use DI\DependencyException;
 use DI\NotFoundException;
 use phpDocumentor\Reflection\DocBlock\Tags\InvalidTag;
